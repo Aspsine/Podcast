fc97ed407da89607504c8049a78e96d2cd8d5785
zhangfan10 <zhangfan10@staff.weibo.com>
2016-09-20 16:03:42 +0800

    provider

---
+++ app/src/main/java/com/aspsine/podcast/ui/featured/item/provider/ProviderViewModel.java
@@ -0,0 +1,10 @@
+package com.aspsine.podcast.ui.featured.item.provider;
+
+import com.aspsine.podcast.widget.recyclerView.item.ItemViewModel;
+
+/**
+ * Created by zhangfan10 on 16/9/19.
+ */
+
+public class ProviderViewModel implements ItemViewModel{
+}
---
+++ app/src/main/java/com/aspsine/podcast/widget/StackView.java
@@ -0,0 +1,24 @@
+package com.aspsine.podcast.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by zhangfan10 on 16/9/19.
+ */
+
+public class StackView extends FrameLayout{
+
+    public StackView(Context context) {
+        super(context);
+    }
+
+    public StackView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StackView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
